f19fa8088a39c1f28361614ec98718c43f966b24
Lydia Kunz <lkunz75@byu.edu>
2026-07-16 18:16:54 -0600

    Passing more tests

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -5,6 +5,7 @@ import dataaccess.DataAccessException;
 import dataaccess.MemoryDataAccess;
 import model.AuthData;
 import model.GameData;
+import model.GameInfo;
 
 import java.util.Collection;
 import java.util.List;
@@ -14,55 +15,49 @@ public class GameService {
     // create this so I can go through the registeredData
     UserService service = new UserService();
     public ListResult list(ListRequest listRequest) throws DataAccessException {
-        try {
-            AuthData.AuthRecord authData = service.registeredData.getAuthData(listRequest.authToken());
-            if (authData == null) {
-                throw new DataAccessException("401 Error: Unauthorized");
-            }
-            else {
-                List<List<String>> games = service.registeredData.listGames();
-                return new ListResult(games);
-            }
-        } catch (DataAccessException e) {
+        AuthData.AuthRecord authData = service.registeredData.getAuthData(listRequest.authToken());
+        if (authData == null) {
             throw new DataAccessException("401 Error: Unauthorized");
+        }
+        else {
+            List<GameInfo> games = service.registeredData.listGames();
+            return new ListResult(games);
         }
     }
 
     public CreateResult create(CreateRequest createRequest) throws DataAccessException {
-        try {
-            AuthData.AuthRecord authData = service.registeredData.getAuthData(createRequest.authToken());
-            if (authData == null) {
-                throw new DataAccessException("401 Error: Unauthorized");
-            }
-            else {
-                if (service.registeredData.getGame(createRequest.gameName()) == null) {
-                    int newID = service.registeredData.newGameID();
-                    GameData game = new GameData(newID, null, null, createRequest.gameName(), null);
-                    service.registeredData.createGame(game);
-                    return new CreateResult(newID);
-                }
-                throw new DataAccessException("401 Error: Unauthorized");
-            }
-        } catch (DataAccessException e) {
+        AuthData.AuthRecord authData = service.registeredData.getAuthData(createRequest.authToken());
+        if (authData == null) {
             throw new DataAccessException("401 Error: Unauthorized");
+        }
+        if (createRequest.gameName() == null){
+            throw new DataAccessException("400 Error: Bad request");
+        }
+        else {
+            if (service.registeredData.getGame(createRequest.gameName()) == null) {
+                int newID = service.registeredData.newGameID();
+                GameData game = new GameData(newID, null, null, createRequest.gameName(), null);
+                service.registeredData.createGame(game);
+                return new CreateResult(newID);
+            }
+            throw new DataAccessException("400 Error: Bad request");
         }
     }
 
     public JoinResult join(JoinRequest joinRequest) throws DataAccessException {
-        try {
-            AuthData.AuthRecord authData = service.registeredData.getAuthData(joinRequest.authToken());
-            if (authData== null) {
-                throw new DataAccessException("401 Error: Unauthorized");
-            }
-            else {
-                if (service.registeredData.getColor(joinRequest.color(), joinRequest.GameID())){
-                    service.registeredData.joinGame(authData.username(), joinRequest.color(), joinRequest.GameID());
-                    return new JoinResult();
-                }
-                throw new DataAccessException("401 Error: Unauthorized");
-            }
-        } catch (DataAccessException e) {
+        AuthData.AuthRecord authData = service.registeredData.getAuthData(joinRequest.authToken());
+        if (authData == null) {
             throw new DataAccessException("401 Error: Unauthorized");
+        }
+        else {
+            if (joinRequest.color() == null){
+                throw new DataAccessException("400 Error: Bad request");
+            }
+            if (service.registeredData.getColor(joinRequest.color(), joinRequest.GameID())){
+                service.registeredData.joinGame(authData.username(), joinRequest.color(), joinRequest.GameID());
+                return new JoinResult();
+            }
+            throw new DataAccessException("403 Error: already taken");
         }
     }
 
---
+++ shared/src/main/java/model/GameInfo.java
@@ -0,0 +1,3 @@
+package model;
+
+public record GameInfo(int gameID, String whiteUsername, String blackUsername, String gameName) {}
