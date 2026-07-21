e87ba4ab9a84a23edb616a036a0f1edebe24864f
Lydia Kunz <lkunz75@byu.edu>
2026-07-21 11:10:14 -0600

    setting up the notation for tables, and learning notation

--- server/src/main/java/dataaccess/MySqlDataAccess.java
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -38,40 +38,39 @@ public class MySqlDataAccess {
     private final String[] createStatements = {
             """
             CREATE TABLE IF NOT EXISTS userData (
-              'username' varchar(256) NOT NULL,
-              'password' varchar(256) NOT NULL,
-              'email' varchar(256) NOT NULL,
-              'json' TEXT DEFAULT NULL,
-              PRIMARY KEY ('username'),
+              `username` varchar(256) NOT NULL,
+              `password` varchar(256) NOT NULL,
+              `email` varchar(256) NOT NULL,
+              `json` TEXT DEFAULT NULL,
+              PRIMARY KEY (`username`),
               INDEX(password)
               INDEX(email)
             ) ENGINE=InnoDB DEFAULT CHARSET=utft8mb4 COLLATE=utf8mb_0900_ai_ci
             """,
             //int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game
-           // do I auto increment here too??
             // how do I declare it's a chess game??
             """
             CREATE TABLE IF NOT EXISTS gameData (
-              'gameID' int NOT NULL AUTO_INCREMENT,
-              'whiteUsername' varchar(256) NOT NULL,
-              'blackUsername' varchar(256) NOT NULL,
-              'gameName' varchar(256) NOT NULL,
-              'game' ENUM(ChessGame),
-              'json' TEXT DEFAULT NULL,
-              PRIMARY KEY ('gameID'),
-              INDEX(whiteUSername)
-              INDEX(blackUSername)
-              INDEX(gameName)
+              `gameID` int,
+              `whiteUsername` varchar(256),
+              `blackUsername` varchar(256),
+              `gameName` varchar(256) NOT NULL,
+              `game` ENUM(ChessGame),
+              `json` TEXT DEFAULT NULL,
+              PRIMARY KEY (`gameID`),
+              INDEX(whiteUSername),
+              INDEX(blackUSername),
+              INDEX(gameName),
               INDEX(game)
             ) ENGINE=InnoDB DEFAULT CHARSET=utft8mb4 COLLATE=utf8mb_0900_ai_ci
             """,
             // String username, String authToken
             """
             CREATE TABLE IF NOT EXISTS authData (
-              'username' varchar(256) NOT NULL,
-              'authToken' varchar(256) NOT NULL,
-              PRIMARY KEY ('username')
-              INDEX(authToken)
+              `username` varchar(256) NOT NULL,
+              `authToken` varchar(256) NOT NULL,
+              PRIMARY KEY (`username`),
+              INDEX(authToken),
             ) ENGINE=InnoDB DEFAULT CHARSET=utft8mb4 COLLATE=utf8mb_0900_ai_ci
             """
     };
