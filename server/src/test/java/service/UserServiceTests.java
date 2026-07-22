ca2c93843f4dde4095447ee3aeb50aeb67bbeb72
Lydia Kunz <lkunz75@byu.edu>
2026-07-21 20:54:21 -0600

    Changed it so I can run my tests with SQL or MemoryDataAccess

--- server/src/test/java/service/UserServiceTests.java
+++ server/src/test/java/service/UserServiceTests.java
@@ -16,6 +16,7 @@ public class UserServiceTests {
     final UserService SERVICE;
 
     public UserServiceTests() {
+
         this.dataAccess = new MemoryDataAccess();
         this.SERVICE = new UserService(dataAccess);
     }
