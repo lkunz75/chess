c497f73a92edd08264a1b52333da5d6f0d1e0204
Lydia Kunz <lkunz75@byu.edu>
2026-07-26 16:38:29 -0700

    Made the create tests

--- client/src/test/client/ServerFacadeTests.java
+++ client/src/test/client/ServerFacadeTests.java
@@ -4,9 +4,11 @@ import dataaccess.DataAccessException;
 import org.junit.jupiter.api.*;
 import server.Server;
 import server.ServerFacade;
+import service.gamerequests.CreateRequest;
 import service.gamerequests.DeleteRequest;
 import service.userrequests.*;
 
+import static java.util.Objects.isNull;
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -101,6 +103,67 @@ public class ServerFacadeTests {
         LogoutRequest logoutRequest = new LogoutRequest(loginData.authToken());
         var logoutData = facade.logout(logoutRequest);
         assertEquals(new LogoutResult() , logoutData);
+    }
+
+    @DisplayName("Negative LogoutTest")
+    @Test
+    void logoutNegative() throws DataAccessException {
+        LogoutRequest logoutRequest = new LogoutRequest("12345gn");
+        try {
+            var logoutData = facade.logout(logoutRequest);
+            fail("Letting someone with an invalid authToken logout");
+        } catch (DataAccessException e) {
+            assertTrue(e.getMessage().contains("Unauthorized"));
+        }
+    }
+
+    // DELETE
+    @DisplayName("Positive Delete")
+    @Test
+    void deletePositive() throws DataAccessException {
+        RegisterRequest request = new RegisterRequest("player1", "password", "p1@email.com");
+        var authData = facade.register(request);
+        DeleteRequest delete = new DeleteRequest();
+        DeleteUserRequest deleteUser = new DeleteUserRequest();
+        facade.delete(delete, deleteUser);
+        LoginRequest loginRequest = new LoginRequest(authData.username(), "password");
+        try {
+            var loginData = facade.login(loginRequest);
+            fail("Not deleting data as it should");
+        } catch (DataAccessException e) {
+            assertTrue(e.getMessage().contains("Unauthorized"));
+        }
+    }
+
+    //CREATE
+    @DisplayName("Positive CreateTest")
+    @Test
+    void createPositive() throws DataAccessException {
+        RegisterRequest request = new RegisterRequest("Jane", "janey", "jane@email.com");
+        var authData = facade.register(request);
+        LoginRequest loginRequest = new LoginRequest(authData.username(), "janey");
+        var loginData = facade.login(loginRequest);
+        CreateRequest createRequest = new CreateRequest(loginData.authToken(), "Jane's Game");
+        var createData = facade.create(createRequest);
+        assertTrue(createData.gameID() > 0);
+    }
+
+    //CREATE
+    @DisplayName("Negative CreateTest")
+    @Test
+    void createNegative() throws DataAccessException {
+        RegisterRequest request = new RegisterRequest("Jane", "janey", "jane@email.com");
+        var authData = facade.register(request);
+        LoginRequest loginRequest = new LoginRequest(authData.username(), "janey");
+        var loginData = facade.login(loginRequest);
+        CreateRequest createRequest = new CreateRequest(loginData.authToken(), "Jane's Game");
+        facade.create(createRequest);
+        try {
+            facade.create(createRequest);
+            fail("Created an already created game");
+        } catch (DataAccessException e) {
+            assertTrue(e.getMessage().contains("Bad"));
+        }
     }
 
 
