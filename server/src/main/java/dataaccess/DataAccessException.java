1428174117105a254e493cb7af66d608da951e45
Lydia Kunz <lkunz75@byu.edu>
2026-07-25 17:30:09 -0700

    Trying to figure out if I add the same things Pet did to my own DataAccessException

--- server/src/main/java/dataaccess/DataAccessException.java
+++ server/src/main/java/dataaccess/DataAccessException.java
@@ -1,5 +1,12 @@
 package dataaccess;
 
+import com.google.gson.Gson;
+import org.eclipse.jetty.http.HttpStatus;
+import service.ErrorMessage;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Indicates there was an error connecting to the database
  */
@@ -9,5 +16,17 @@ public class DataAccessException extends Exception{
     }
     public DataAccessException(String message, Throwable ex) {
         super(message, ex);
+    }
+
+    public String toJson() {
+        int code = Integer.parseInt(getMessage().substring(0,3));
+        return new Gson().toJson(Map.of("message", getMessage(), "status", code));
+    }
+
+    public static DataAccessException fromJson(String json) {
+        var map = new Gson().fromJson(json, HashMap.class);
+        var status = HttpStatus.Code.valueOf(map.get("status").toString());
+        String message = map.get("message").toString();
+        return new DataAccessException(message);
     }
 }
