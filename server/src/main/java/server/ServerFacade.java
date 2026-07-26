9bbfb69d60f69d093dfbe7e4e5945d984f240c8e
Lydia Kunz <lkunz75@byu.edu>
2026-07-25 17:01:24 -0700

    Created all Facade Methods that interact/correspond to the Server class.

--- server/src/main/java/server/ServerFacade.java
+++ server/src/main/java/server/ServerFacade.java
@@ -4,6 +4,9 @@ import com.google.gson.Gson;
 
 import dataaccess.*;
 import service.*;
+import service.gamerequests.*;
+import service.userrequests.*;
+
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -18,9 +21,53 @@ public class ServerFacade {
     }
 
 
-    
-    
-    
+    public RegisterResult register(RegisterRequest registerRequest) throws DataAccessException {
+        var request = buildRequest("POST", "/user", registerRequest);
+        var response = sendRequest(request);
+        return handleResponse(response, RegisterResult.class);
+    }
+
+    public LoginResult login(LoginRequest loginRequest) throws DataAccessException {
+        var request = buildRequest("POST", "/session", loginRequest);
+        var response = sendRequest(request);
+        return handleResponse(response, LoginResult.class);
+    }
+
+    public LogoutResult logout(LogoutRequest logoutRequest) throws DataAccessException {
+        var request = buildRequest("DELETE", "/session", logoutRequest);
+        var response = sendRequest(request);
+        return handleResponse(response, LogoutResult.class);
+    }
+
+    public DeleteUserResult deleteUser(DeleteUserRequest deleteUserRequest) throws DataAccessException {
+        var request = buildRequest("DELETE", "db", deleteUserRequest);
+        var response = sendRequest(request);
+        return handleResponse(response, DeleteUserResult.class);
+    }
+
+    public DeleteResult delete(DeleteRequest deleteRequest) throws DataAccessException {
+        var request = buildRequest("DELETE", "db", deleteRequest);
+        var response = sendRequest(request);
+        return handleResponse(response, DeleteResult.class);
+    }
+
+    public CreateResult create(CreateRequest createRequest) throws DataAccessException {
+        var request = buildRequest("POST", "/game", createRequest);
+        var response = sendRequest(request);
+        return handleResponse(response, CreateResult.class);
+    }
+
+    public ListResult list(ListRequest listRequest) throws DataAccessException {
+        var request = buildRequest("GET", "/game", listRequest);
+        var response = sendRequest(request);
+        return handleResponse(response, ListResult.class);
+    }
+
+    public JoinResult join(JoinRequest joinRequest) throws DataAccessException {
+        var request = buildRequest("PUT", "/game", joinRequest);
+        var response = sendRequest(request);
+        return handleResponse(response, JoinResult.class);
+    }
     
     // followed PetShop
     private HttpRequest buildRequest(String method, String path, Object body) {
@@ -43,6 +90,7 @@ public class ServerFacade {
     
     private HttpResponse<String> sendRequest(HttpRequest request) throws DataAccessException {
         try {
+            // figure out what send should be!
             return client.send(request, HttpResponse.BodyHandlers.ofString());
         } catch (DataAccessException e) {
             throw new DataAccessException(e.getMessage());
@@ -54,6 +102,7 @@ public class ServerFacade {
         if (!isSuccessful(status)) {
             var body = response.body();
             if (body!= null) {
+                // figure out how to do this!
                 throw DataAccessException.fromJson(body);
             }
             throw new DataAccessException(DataAccessException.fromHttpStatusCode(status));
@@ -67,6 +116,4 @@ public class ServerFacade {
     private boolean isSuccessful(int status) {
         return status / 100 == 2;
     }
-    
-    
 }
